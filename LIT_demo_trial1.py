011e41630c4a65083c0e2bb9ddb9bf8ab81a09eb
ANKAN <76246118+dylanesq@users.noreply.github.com>
2025-06-16 17:03:43 +0530

    Update LIT_demo_trial1.py

--- LIT_demo_trial1.py
+++ LIT_demo_trial1.py
@@ -4,3 +4,6 @@ import numpy as np
 with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
+    
+uploaded_file = st.file_uploader("Choose a file")    
+    
